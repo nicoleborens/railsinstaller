748f88575cad257861a72f4e32baf5120e75c788
nicole borens <nicole.borens@gmail.com>
2013-11-08 18:54:30 -0600

    Added welcome controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsinstallerOsxDemo::Application.routes.draw do
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +54,7 @@ RailsinstallerOsxDemo::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+root :to => 'welcome#index'
+
 end
